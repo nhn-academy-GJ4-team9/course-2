f0ce20ea77d8bc65f956f3baa00b32463d8b3669
fakedrakedog <143066707+fakedrakedog@users.noreply.github.com>
2023-10-17 18:18:29 +0900

    Add files via upload

---
+++ exercise/김현중/Unit_2/Ex2_1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Ex2_1 {
+    public static void main(String[] args) {
+        System.out.println("           ******           *************        **********");
+        System.out.println("           **    **                **            **");
+        System.out.println("           **     **               **            **");
+        System.out.println("           **      **              **            **");
+        System.out.println("           **      **              **            ********");
+        System.out.println("           **      **       **     **            **");
+        System.out.println("           **     **         **    **            **");
+        System.out.println("           **    **           **  **             **");
+        System.out.println("           *****               ****              **********");
+    }
+}
