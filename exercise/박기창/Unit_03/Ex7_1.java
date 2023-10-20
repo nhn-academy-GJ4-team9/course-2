e456fc13ac4d49c87af4da3993fa724640509fd0
gichangee <93646339+gichangee@users.noreply.github.com>
2023-10-20 09:46:52 +0900

    Update Ex7_1.java

--- exercise/박기창/Unit_03/Ex7_1.java
+++ exercise/박기창/Unit_03/Ex7_1.java
@@ -17,7 +17,7 @@ public class Ex7_1 {
             int birthday;
             birthday = (int) (Math.random() * 365);
             count++;
-            System.out.printf("Person %d has birthday number %d%n", count, birthday + 1);
+            System.out.printf("Person %d has birthday number %d%n", count, birthday);
 
             used[birthday]++;
 
