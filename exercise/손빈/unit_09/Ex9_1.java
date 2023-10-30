a4ef5584b227703b406d4b5a2ae6a26dc2a82357
sonbean <143383607+sonbean@users.noreply.github.com>
2023-10-31 08:50:14 +0900

    추가 손빈unit_09

---
+++ exercise/손빈/unit_09/Ex9_1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Ex9_1 {
+    public static int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int value = fibonacci(n);
+        System.out.println(value);
+    }
+}
